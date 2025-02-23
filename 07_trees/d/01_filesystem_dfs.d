29e1f6bbfdb7bc67de2378bd6b4bf854ecfe7ca7
Sergei Giniatulin <kornburn@gmail.com>
2025-02-23 23:20:35 +0200

    Create 01_filesystem_dfs.d
    Filesystem example in D

---
+++ 07_trees/d/01_filesystem_dfs.d
@@ -0,0 +1,17 @@
+import std;
+
+void printnames(string dir)
+{
+    foreach(DirEntry name; dirEntries(dir, SpanMode.shallow))
+    {
+        if (name.isFile)
+            writeln(name);
+        else
+            printnames(name);
+    }
+}
+
+void main()
+{
+    printnames("test1");
+}
