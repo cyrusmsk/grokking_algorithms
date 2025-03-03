c2a33c615589c02436c35b0d6fbdfcb9055d4a22
Sergei Giniatulin <kornburn@gmail.com>
2025-03-03 18:05:15 +0200

    Create 01_longest_common_sub.d
    Added dynamic programming example for D language.
    The code is based on Rust example

---
+++ 11_dynamic_programming/d/01_longest_common_sub.d
@@ -0,0 +1,51 @@
+import std;
+
+alias DpTable = int[][];
+
+DpTable longest_common_substring(T)(T[] table1, T[] table2)
+{
+    DpTable dp_table = new DpTable(table1.length, table2.length);
+
+    foreach(int i, c1; table1)
+        foreach(int j, c2; table2)
+        {
+            if (c1 == c2)
+                dp_table[i][j] = dp_table[max(i-1,0)][max(j-1,0)] + 1;
+            else
+                dp_table[i][j] = 0;
+        }
+    return dp_table;
+}
+
+
+DpTable longest_common_subsequence(T)(T[] table1, T[] table2)
+{
+    DpTable dp_table = new DpTable(table1.length, table2.length);
+
+    foreach(int i, c1; table1)
+        foreach(int j, c2; table2)
+        {
+            if (c1 == c2)
+                dp_table[i][j] = dp_table[max(i-1,0)][max(j-1,0)] + 1;
+            else
+            {
+                dp_table[i][j] = max(dp_table[max(i-1,0)][j],
+                                     dp_table[i][max(j-1,0)]);
+            }
+        }
+    return dp_table;
+}
+
+void main()
+{
+    auto dp_table_blue = "blue";
+    auto dp_table_clues = "clues";
+    
+    writeln("Longest substring:");
+    foreach(line; longest_common_substring(dp_table_blue.array, dp_table_clues.array))
+        writeln(line);
+
+    writeln("Longest subsequence:");
+    foreach(line; longest_common_subsequence(dp_table_blue.array, dp_table_clues.array))
+        writeln(line);
+}
